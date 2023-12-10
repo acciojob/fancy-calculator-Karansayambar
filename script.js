057b0f7d55cf3edda0e74dcc06004112a0983816
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-10 12:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let first = prompt("Ente a first number");
+first = Number(first) * 2;
+let second = prompt("Enter a second number");
+second = Number(second);
+alert(`The final result is: ${first + second}`);
